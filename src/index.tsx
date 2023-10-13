b5a5bbd18ec7f11b0cbb50600bdbe6a02613177d
Pavl0ffN <pavl0ffn94@gmail.com>
2023-10-13 12:31:55 +0400

    fix react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from "react-dom/client";
 import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
 import './index.css';
 import {App} from './App';
 
 import {store} from './store'
 
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as HTMLDivElement;
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
